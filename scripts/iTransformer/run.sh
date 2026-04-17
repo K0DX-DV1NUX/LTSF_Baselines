145e4ec06faee24eb9952dc7e18804748427f3ca
aditya dey <adiitya.dey@gmail.com>
2026-04-17 11:36:52 +0200

    iTransformer ready for run.

--- scripts/iTransformer/run.sh
+++ scripts/iTransformer/run.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-python -u runExp.py --config "scripts/iTransformer/toml/ETTh1.toml"
-python -u runExp.py --config "scripts/iTransformer/toml/ETTh2.toml"
-python -u runExp.py --config "scripts/iTransformer/toml/ETTm1.toml"
-python -u runExp.py --config "scripts/iTransformer/toml/ETTm2.toml"
-python -u runExp.py --config "scripts/iTransformer/toml/WTH.toml"
-python -u runExp.py --config "scripts/iTransformer/toml/ELT.toml"
-python -u runExp.py --config "scripts/iTransformer/toml/TRF.toml"
+model_config="scripts/iTransformer/toml/model.toml"
+data_config="scripts/defaults/"
+gpu_config="scripts/defaults/gpu.toml"
+
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTh1.toml" --model_config $model_config
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTh2.toml" --model_config $model_config
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTm1.toml" --model_config $model_config
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTm2.toml" --model_config $model_config
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/WTH.toml" --model_config $model_config
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ELT.toml" --model_config $model_config
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/TRF.toml" --model_config $model_config
