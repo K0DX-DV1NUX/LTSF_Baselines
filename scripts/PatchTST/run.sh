a58eeec0e4b45f61b325f2a6729f6a503f961240
aditya dey <adiitya.dey@gmail.com>
2026-04-17 12:03:38 +0200

    PatchTST ready for run.

--- scripts/PatchTST/run.sh
+++ scripts/PatchTST/run.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-python -u runExp.py --config "scripts/PatchTST/toml/ETTh1.toml"
-python -u runExp.py --config "scripts/PatchTST/toml/ETTh2.toml"
-python -u runExp.py --config "scripts/PatchTST/toml/ETTm1.toml"
-python -u runExp.py --config "scripts/PatchTST/toml/ETTm2.toml"
-python -u runExp.py --config "scripts/PatchTST/toml/WTH.toml"
-python -u runExp.py --config "scripts/PatchTST/toml/ELT.toml"
-python -u runExp.py --config "scripts/PatchTST/toml/TRF.toml"
+model_config="scripts/PatchTST/toml/"
+data_config="scripts/defaults/"
+gpu_config="scripts/defaults/gpu.toml"
+
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTh1.toml" --model_config "$model_config/model1.toml"
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTh2.toml" --model_config "$model_config/model1.toml"
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTm1.toml" --model_config "$model_config/model2.toml"
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTm2.toml" --model_config "$model_config/model2.toml"
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/WTH.toml" --model_config "$model_config/model2.toml"
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ELT.toml" --model_config "$model_config/model2.toml"
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/TRF.toml" --model_config "$model_config/model2.toml"
