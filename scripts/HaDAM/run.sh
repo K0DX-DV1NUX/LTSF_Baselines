a894a9067000de229f702def91d8cf7e80d00dab
aditya dey <adiitya.dey@gmail.com>
2026-05-05 10:03:57 +0200

    new datasets added.

--- scripts/HaDAM/run.sh
+++ scripts/HaDAM/run.sh
@@ -4,10 +4,14 @@ model_config="scripts/HaDAM/toml/model.toml"
 data_config="scripts/defaults/"
 gpu_config="scripts/defaults/gpu.toml"
 
-python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTh1.toml" --model_config $model_config
-python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTh2.toml" --model_config $model_config
-python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTm1.toml" --model_config $model_config
-python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTm2.toml" --model_config $model_config
-python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/WTH.toml" --model_config $model_config
+# python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTh1.toml" --model_config $model_config
+# python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTh2.toml" --model_config $model_config
+# python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTm1.toml" --model_config $model_config
+# python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTm2.toml" --model_config $model_config
+# python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/WTH.toml" --model_config $model_config
+
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/SOLTX.toml" --model_config $model_config
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/SOLAL.toml" --model_config $model_config
 python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ELT.toml" --model_config $model_config
-python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/TRF.toml" --model_config $model_config
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/METRLA.toml" --model_config $model_config
+python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/PEMSBAY.toml" --model_config $model_config
