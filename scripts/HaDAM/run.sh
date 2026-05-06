cb3f97ad82200a22bad7ab2300401f7cc3c9cbb4
aditya dey <adiitya.dey@gmail.com>
2026-05-06 15:10:46 +0200

    SOLTX, SOLAL, ELT, METRLA, PEMSBAY are set for testing.

--- scripts/HaDAM/run.sh
+++ scripts/HaDAM/run.sh
@@ -9,7 +9,6 @@ gpu_config="scripts/defaults/gpu.toml"
 # python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTm1.toml" --model_config $model_config
 # python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ETTm2.toml" --model_config $model_config
 # python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/WTH.toml" --model_config $model_config
-
 python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/SOLTX.toml" --model_config $model_config
 python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/SOLAL.toml" --model_config $model_config
 python -u runExp.py --gpu_config $gpu_config --data_config "$data_config/ELT.toml" --model_config $model_config
